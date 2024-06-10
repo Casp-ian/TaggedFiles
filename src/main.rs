dbff6e8c1987c4263c615922c7594dfc62467a4b
caspian <caspian@koolwaaij.nl>
2024-06-10 15:45:44 +0200

    implement addfile link, copy and move options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::fs;
+use std::io;
+use std::path::Path;
+use std::path::PathBuf;
 use std::process::ExitCode;
+use symlink::{self, symlink_auto};
 
 mod cli;
 use crate::cli::parse::SubCommands;
@@ -21,7 +26,7 @@ macro_rules! unwrapOrFailure {
 
 pub fn main() -> ExitCode {
     // create config if it doesnt exist
-    if config::exists() {
+    if !config::exists() {
         eprintln!("config file does not exit. Creating it now");
         let mut directory_path = dirs::home_dir().unwrap();
         directory_path.push("tagged");
@@ -32,7 +37,7 @@ pub fn main() -> ExitCode {
     let config = unwrapOrFailure!(config::read());
 
     // create db if it doesnt exist
-    if db_exists(config.clone().directory) {
+    if !db_exists(config.clone().directory) {
         eprintln!("Database does not exit. Creating it now");
         let config = config::read().unwrap();
         unwrapOrFailure!(setup(config.directory));
@@ -63,9 +68,28 @@ pub fn main() -> ExitCode {
         SubCommands::Addfile { file_path, option } => {
             // TODO actually move or the file to the directory based on `option`
 
+            let temp = file_path.clone();
+            let file_name = temp.file_name().unwrap();
+            let mut final_file_path = PathBuf::new();
+            final_file_path.push(config.clone().directory);
+            final_file_path.push(file_name);
+
+            match option {
+                parse::AddFileOptions::Link => {
+                    dbg!(symlink_auto(file_path, final_file_path));
+                }
+                parse::AddFileOptions::Move => {
+                    dbg!(better_copy(file_path.clone(), final_file_path));
+                    dbg!(better_delete(file_path));
+                }
+                parse::AddFileOptions::Copy => {
+                    dbg!(better_copy(file_path, final_file_path));
+                }
+            }
+
             unwrapOrFailure!(add_file(
                 config.clone().directory,
-                &file_path.to_str().unwrap().to_owned() // TODO see if you can make this less ugly everytime
+                &file_name.to_str().unwrap().to_owned() // TODO see if you can make this less ugly everytime
             ));
         }
         SubCommands::Addtag { names } => {
@@ -92,4 +116,29 @@ pub fn main() -> ExitCode {
     }
 
     ExitCode::SUCCESS
+}
+
+fn better_delete(dst: impl AsRef<Path>) -> io::Result<()> {
+    if dst.as_ref().is_file() {
+        return fs::remove_file(dst);
+    }
+    fs::remove_dir_all(dst)
+}
+
+fn better_copy(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<u64> {
+    if src.as_ref().is_file() {
+        return fs::copy(src, dst);
+    }
+
+    fs::create_dir_all(&dst)?;
+    for entry in fs::read_dir(src)? {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        if ty.is_dir() {
+            better_copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+        }
+    }
+    Ok(0)
 }
